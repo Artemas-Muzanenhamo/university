6fc425b144662b238726da40cc65e3ac7ea43503
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-24 12:49:22 +0100

    add List<String> subjects

--- tutor-service/src/main/java/com/tutors/tutorservice/domain/Tutor.java
+++ tutor-service/src/main/java/com/tutors/tutorservice/domain/Tutor.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -14,7 +14,9 @@ import javax.persistence.Id;
 public class Tutor {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
     private String surname;
-    private String subjects;
+    @ElementCollection
+    private List<String> subjects;
 }
