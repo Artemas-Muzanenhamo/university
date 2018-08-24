f7090c2d18d9e7076ffa45d4c9c32bc2d0eb0e70
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-24 15:10:35 +0100

    add degree service

---
+++ degree-service/src/main/java/com/degrees/degree/domain/Degree.java
@@ -0,0 +1,63 @@
+package com.degrees.degree.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Degree {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String category;
+    private String honors;
+
+    public Degree() {
+    }
+
+    public Degree(String name, String category, String honors) {
+        this.name = name;
+        this.category = category;
+        this.honors = honors;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getHonors() {
+        return honors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Degree degree = (Degree) o;
+
+        if (id != null ? !id.equals(degree.id) : degree.id != null) return false;
+        if (name != null ? !name.equals(degree.name) : degree.name != null) return false;
+        if (category != null ? !category.equals(degree.category) : degree.category != null) return false;
+        return honors != null ? honors.equals(degree.honors) : degree.honors == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        result = 31 * result + (honors != null ? honors.hashCode() : 0);
+        return result;
+    }
+}
--- subject-service/src/main/java/com/subjects/subjectservice/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.subjects.subjectservice.repository;
-
-import com.subjects.subjectservice.domain.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, String> {
-}
