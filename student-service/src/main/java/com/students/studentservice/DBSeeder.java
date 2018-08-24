0188e9a036777d7c3776f90540140239bcdbfe78
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-24 15:51:40 +0100

    polish up

--- student-service/src/main/java/com/students/studentservice/DBSeeder.java
+++ student-service/src/main/java/com/students/studentservice/DBSeeder.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class DBSeeder implements CommandLineRunner {
@@ -21,51 +20,48 @@ public class DBSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        Map<String, String> artemasDegree = new HashMap<>();
-
         Student artemas = new Student(
                 "Artemas", "Muzanenhamo", 28, new HashMap<String, String>() {
-                    {
-                        put("Software Engineering", "BSc");
-                    }
-                }
+            {
+                put("Software Engineering", "BSc");
+            }
+        }
 
         );
 
         Student thomas = new Student(
                 "Thomas", "Welbeck", 40, new HashMap<String, String>() {
-                    {
-                        put("Computer Science", "BSc");
-                        put("Accounting", "MSc");
-                    }
-                }
+            {
+                put("Computer Science", "BSc");
+                put("Accounting", "MSc");
+            }
+        }
         );
 
         Student william = new Student(
                 "William", "Dorothy", 20, new HashMap<String, String>() {
-                    {
-                        put("Geology", "BSc");
-                    }
-                }
+            {
+                put("Geology", "BSc");
+            }
+        }
         );
 
         Student james = new Student(
                 "James", "Spence", 55, new HashMap<String, String>() {
-                    {
-                        put("Law", "BSc");
-                        put("Criminal Law", "MSc");
-                        put("Judicial Law", "PHD");
-                    }
-                }
+            {
+                put("Law", "BSc");
+                put("Criminal Law", "MSc");
+                put("Judicial Law", "PHD");
+            }
+        }
         );
 
         Student rob = new Student(
                 "Rob", "Williams", 30, new HashMap<String, String>() {
-                    {
-                        put("Pure Mathematics", "BSc");
-                    }
-                }
+            {
+                put("Pure Mathematics", "BSc");
+            }
+        }
         );
 
         // Drop the whole existing collection
