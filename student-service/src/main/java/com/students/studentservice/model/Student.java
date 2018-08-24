f9513fb4b9ec7da3e65bca077308ba41cccefba7
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-24 09:45:24 +0100

    add student service

--- student-service/src/main/java/com/students/studentservice/model/Student.java
+++ student-service/src/main/java/com/students/studentservice/model/Student.java
@@ -1,18 +1,67 @@
 package com.students.studentservice.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     private String surname;
     private int age;
-    private List<String> subjects;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (age != student.age) return false;
+        if (id != null ? !id.equals(student.id) : student.id != null) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        return  (surname != null ? !surname.equals(student.surname) : student.surname != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
 }
--- student-service/src/main/java/com/students/studentservice/repository/StudentRepository.java
+++ student-service/src/main/java/com/students/studentservice/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.students.studentservice.repository;
 
 import com.students.studentservice.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
 }
