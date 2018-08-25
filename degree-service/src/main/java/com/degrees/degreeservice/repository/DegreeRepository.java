75cbeb7ad2240c089f571e8fd62203329a9684b3
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-25 13:09:14 +0100

    removed Rest resources for delete APIs

--- degree-service/src/main/java/com/degrees/degreeservice/repository/DegreeRepository.java
+++ degree-service/src/main/java/com/degrees/degreeservice/repository/DegreeRepository.java
@@ -3,6 +3,7 @@ package com.degrees.degreeservice.repository;
 import com.degrees.degreeservice.domain.Degree;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
@@ -13,4 +14,12 @@ public interface DegreeRepository extends JpaRepository<Degree, Integer> {
     List<Degree> findAllByCategoryIgnoreCaseContaining(@Param("category") String category);
 
     List<Degree> findDegreesByHonorsIgnoreCase(@Param("honors") String honors);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Degree degree);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
