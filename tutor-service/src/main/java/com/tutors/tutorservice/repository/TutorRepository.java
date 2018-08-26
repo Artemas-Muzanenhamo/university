86d9cb45c798e63e0593ed82e5afe9f49d990035
Artemas Muzanenhamo <artemasseng@gmail.com>
2018-08-26 09:21:57 +0100

    add custom queries for Tutor Service

--- tutor-service/src/main/java/com/tutors/tutorservice/repository/TutorRepository.java
+++ tutor-service/src/main/java/com/tutors/tutorservice/repository/TutorRepository.java
@@ -2,6 +2,23 @@ package com.tutors.tutorservice.repository;
 
 import com.tutors.tutorservice.domain.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 public interface TutorRepository extends JpaRepository<Tutor, String> {
+    List<Tutor> findTutorsByNameIgnoreCaseContains(@Param("name") String name);
+
+    List<Tutor> findTutorsByNameIgnoreCaseContainingAndSurnameIgnoreCaseContaining(@Param("name") String name, @Param("surname") String surname);
+
+    List<Tutor> findTutorsByDegreesIgnoreCaseIn(@Param("degree") List<String> degree);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tutor entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
