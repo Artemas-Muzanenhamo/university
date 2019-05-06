26cc6821f326203cd85648db10635700deda4766
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-05-06 09:11:07 +0100

    add Router component

--- university-ui/src/index.js
+++ university-ui/src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
+import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
-    <App />,
+    <Router>
+        <App />
+    </Router>,
     document.getElementById('root')
 );
 registerServiceWorker();
