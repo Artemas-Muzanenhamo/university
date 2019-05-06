b6a6687ebade2659853836c2409ba3b9a0e299c3
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-05-06 10:35:51 +0100

    add UIKit

--- university-ui/src/index.js
+++ university-ui/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
 import App from "./App";
 import {BrowserRouter as Router} from "react-router-dom";
+import 'uikit';
 
 ReactDOM.render(
     <Router>
